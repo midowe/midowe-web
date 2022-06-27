9dfe0543e894aeecf24f3e1190e512efe47d88f9
Americo Chaquisse <achaquisse1@gmail.com>
2022-06-27 11:44:52 +0200

    pipeline setup

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ RUN yarn build
 
 ENV NUXT_HOST=0.0.0.0
 ENV NUXT_PORT=3000
-ENV NODE_ENV=production
+ENV NODE_ENV=prd
 
 EXPOSE 3000 
 
--- clients/_base-client.ts
+++ clients/_base-client.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || "dev";
 
 const urls = {
-	development: {
+	dev: {
 		cms: "https://cms.dev.midowe.co.mz/api",
 		accouting:
 			"https://eugqgyjdksk2hoi7rj6b23zjti0grskw.lambda-url.af-south-1.on.aws",
 	},
-	production: {
+	prd: {
 		cms: "https://cms.midowe.co.mz/api",
 		accouting:
 			"https://xlhofsmgf4o3xtpleqam77sd240braxu.lambda-url.af-south-1.on.aws",
