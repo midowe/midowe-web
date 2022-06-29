ccaf32718ee8943a1ecd009db547283e284e2940
Americo Chaquisse <achaquisse1@gmail.com>
2022-06-29 19:03:06 +0200

    update pipelone and build process

--- .iac/variables.tf
+++ .iac/variables.tf
@@ -13,11 +13,3 @@ variable "registry_username" {
 
 variable "registry_password" {
 }
-
-variable "endpoint_cms" {
-  default = "https://cms.dev.midowe.co.mz/api"
-}
-
-variable "endpoint_accouting" {
-  default = "https://eugqgyjdksk2hoi7rj6b23zjti0grskw.lambda-url.af-south-1.on.aws"
-}
--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,6 @@ RUN mkdir -p /usr/src/midowe-web
 WORKDIR /usr/src/midowe-web
 COPY ./ .
 
-RUN rm -rf node_modules && yarn install
-RUN yarn build
-
 ENV NUXT_HOST=0.0.0.0
 ENV NUXT_PORT=3000
 ENV NODE_ENV=prd
